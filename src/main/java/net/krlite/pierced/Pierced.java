a53387ae462da26fe7eedad273b7b100da9c0f26
KrLite <68179735+KrLite@users.noreply.github.com>
2024-01-14 17:40:35 +0800

    Update Pierced.java

--- src/main/java/net/krlite/pierced/Pierced.java
+++ src/main/java/net/krlite/pierced/Pierced.java
@@ -1,4 +1,53 @@
 package net.krlite.pierced;
 
-public class Pierced {
+import com.moandjiezana.toml.Toml;
+import com.moandjiezana.toml.TomlWriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public abstract class Pierced {
+    protected final File file;
+    protected HashMap<String, Object> map;
+
+    protected Pierced(File file) {
+        this.file = file;
+        this.map = new HashMap<>();
+    }
+
+    protected File file() {
+        return file;
+    }
+
+    protected TomlWriter writer() {
+        return new TomlWriter.Builder()
+                .indentTablesBy(2)
+                .build();
+    }
+
+    protected boolean createFile() {
+        return file().mkdirs();
+    }
+
+    protected boolean deleteFile() {
+        return file().delete();
+    }
+
+    protected boolean readFromFile() {
+        if (file().exists()) {
+            map = new HashMap<>(new Toml().read(file()).toMap());
+            return true;
+        } else return false;
+    }
+
+    protected boolean writeToFile() {
+        try {
+            createFile();
+            writer().write(map, file());
+            return true;
+        } catch (IOException ignored) {
+            return false;
+        }
+    }
 }
